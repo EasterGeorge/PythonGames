7669a07ea702fdf8628208f40730d43f03dfbcb6
Easter George <33330733+EasterGeorge@users.noreply.github.com>
2020-04-01 22:06:21 +0300

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,22 @@
+""""
+The program will first prompt the user for a series of inputs a la Mad Libs.
+For example, a singular noun, an adjective, etc. Then, once all the information has been
+inputted, the program will take that data and place them into a premade story template.
+ You’ll need prompts for user input, and to then print out the full story at the end with the input included.
+
+"""
+
+noun = input("Noun: \n")
+verb_1 = input(" First verb \n")
+verb_2 = input(" Second Verb \n")
+verb_3 = input(" Third verb \n")
+adjective = input(" Adjective \n")
+
+
+print("I am a " + noun + " who " + verb_1 + " and " + verb_2 +" occassionally \n")
+print(" I act " + adjective + " all the time to " + verb_3 + "myself")
+
+
+
+
+
